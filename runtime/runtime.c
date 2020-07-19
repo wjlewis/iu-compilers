7f770fb74571a45d5f8199d48ba0cf30537e0c66
William Lewis <william.lewis112358@gmail.com>
2020-07-19 05:39:44 -0400

    Switch to Rust

--- runtime/runtime.c
+++
@@ -1,9 +0,0 @@
-#include <inttypes.h>
-#include <stdio.h>
-#include "runtime.h"
-
-int64_t read_int() {
-        int64_t i;
-        scanf("%" SCNd64, &i);
-        return i;
-}
--- runtime/runtime.h
+++
@@ -1,8 +0,0 @@
-#include <inttypes.h>
-
-#ifndef RUNTIME_H
-#define RUNTIME_H
-
-int64_t read_int(void);
-
-#endif
